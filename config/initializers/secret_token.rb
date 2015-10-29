666b3900fba0a1724f4ff844bbecf4c703a695c9
Angelica <angelica.hom@gmail.com>
2015-10-29 05:23:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fhquiz::Application.config.secret_key_base = '2d8a85fb0572517fdb2e313c71f1b0bdf0d9bce42fda507991c47da786c8ba22d517cbd204ab051389d3ce84ade40ce6852e35d5503dccf397297bece20d2462'
