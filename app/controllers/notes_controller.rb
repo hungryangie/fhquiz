d36d9d69a16ef9db40ef348332f38e4635839124
Angelica <angelica.hom@gmail.com>
2015-10-29 06:24:54 +0000

    Making the submit button add to notes database

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,4 +6,15 @@ class NotesController < ApplicationController
 	def new
 		@note = Note.new
 	end
+
+	def create
+		Note.create(note_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def note_params
+		params.require(:note).permit(:message, :name)
+	end
 end
